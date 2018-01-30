9dbe6bee94fdf66608940ca699d70ca3b7265f7e
VictorMartinezViol <victormavi_1995@hotmail.com>
2018-01-30 10:32:30 +0100

    cleaned directive

---
+++ src/common/accordion/slide-toggle.directive.js
@@ -0,0 +1,33 @@
+(function() {
+	'use strict';
+	// 'accordion' module, extracted from https://codepen.io/anayarojo/pen/JRmvAW
+	angular.module('accordion').directive('slideToggle', function() {
+		return {
+			restrict: 'A',
+			link: function(scope, element, attrs) {
+				var target = document.querySelector(attrs.slideToggle);
+				attrs.expanded = false;
+				element.bind('click', function() {
+					//optional
+					if (element.hasClass('opened')) {
+						element.removeClass('opened');
+					} else {
+						element.addClass('opened');
+					}
+					//
+
+					var content = target.querySelector('.slideable_content');
+					if (!attrs.expanded) {
+						content.style.border = '1px solid rgba(0,0,0,0)';
+						var y = content.clientHeight;
+						content.style.border = 0;
+						target.style.height = y + 'px';
+					} else {
+						target.style.height = '0px';
+					}
+					attrs.expanded = !attrs.expanded;
+				});
+			}
+		};
+	});
+})();
