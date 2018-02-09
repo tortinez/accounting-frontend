c17fd83515a9c5bc995deba37ca94cb215882049
VictorMartinezViol <victormavi_1995@hotmail.com>
2018-02-09 20:09:07 +0100

    Create webpack.config

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,17 +1,57 @@
 (function() {
 	'use strict';
 
+	//vendor module imports
+	import angular from 'angular';
+	import ngAnimate from 'angular-animate';
+	import ngMaterial from 'angular-material';
+	import ngRoute from 'angular-route'
+	import ngMessages from 'angular-messages'
+
+	//3rd party module imports
+	import angularMoment from 'moment';
+	import mdColorMenu from 'md-color-menu';
+	import md.data.table from 'angular-material-data-table';
+
+	//custom module imports
+	import accordion from "../common/directives/accordion/accordion.module";
+	import validate from "../common/directives/validate/validate.module";
+	import common from "../common/common.module";
+	//(from here, app pages)
+	import authForm from "./auth-form/auth-form.module";
+	import accountInfo from "./account-info/account-info.module";
+	import purchaseTable from "./purchase-table/purchase-table.module.js"
+  import purchaseForm from "./purchase-form/purchase-form.module.js"
+  import purchaseStatusTable from "./purchase-status-table/purchase-status-table.module.js"
+  import purchaseTypeTable from "./purchase-type-table/purchase-type-table.module.js"
+  import projectTable from "./project-table/project-table.module.js"
+  import projectTypeTable from "./project-type-table/project-type-table.module.js"
+  import clientTable from "./client-table/client-table.module.js"
+  import clientTypeTable from "./client-type-table/client-type-table.module.js"
+  import supplierTable from "./supplier-table/supplier-table.module.js"
+  import employeeTable from "./employee-table/employee-table.module.js"
+  import userTable from "./user-table/user-table.module.js"
+
+	//config imports
+	import { routingConfig, themingConfig, dateConfig, onChangeRun } from "./app.config";
+
+
 	// Define the 'AccountingFEapp' module
 	angular.module('AccountingFEapp', [
+		//vendor modules
 		'ngAnimate',
 		'ngMaterial',
 		'ngRoute',
 		'ngMessages',
+
+		//3rd Party modules
 		'angularMoment',
 		'mdColorMenu',
+		'md.data.table',
+
+		//custom modules
 		'accordion',
 		'validate',
-		'md.data.table',
 		'common',
 		'authForm',
 		'accountInfo',
