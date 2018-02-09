e0fbf9b05d621240910efd130be5b64c98d6b17a
VictorMartinezViol <victormavi_1995@hotmail.com>
2018-02-09 08:56:01 +0100

    extract MainController form app.module. Clean it and avoid using $scope

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -2,145 +2,29 @@
 	'use strict';
 
 	// Define the 'AccountingFEapp' module
-	angular
-		.module('AccountingFEapp', [
-			'ngAnimate',
-			'ngMaterial',
-			'ngRoute',
-			'ngMessages',
-			'angularMoment',
-			'mdColorMenu',
-			'accordion',
-			'validate',
-			'md.data.table',
-			'common',
-			'authForm',
-			'accountInfo',
-			'purchaseTable',
-			'purchaseForm',
-			'purchaseTypeTable',
-			'purchaseStatusTable',
-			'projectTable',
-			'projectTypeTable',
-			'clientTable',
-			'clientTypeTable',
-			'supplierTable',
-			'employeeTable',
-			'userTable'
-		])
-		.controller('mainCtrl', function($scope, $location, Auth, $mdSidenav) {
-			// execute on init
-			var init = function() {
-				Auth.isAuthenticated().then(function() {
-					$scope.user = Auth.user;
-					if ($scope.user.isLogged) $location.path('/purchases');
-				});
-			};
-			init();
-
-			//Related to the account info & logout menu
-			$scope.showAccountInfo = function() {
-				$location.path('/account-info');
-			};
-
-			$scope.handleLogout = function() {
-				Auth.logout().then(
-					function(res) {},
-					function(err) {
-						console.log('Logout performed succesfully');
-						Auth.user.isLogged = false;
-						$location.path('/login');
-					}
-				);
-			};
-
-			//Related to the sidenav
-			$scope.toggleNavBar = buildToggler('left');
-
-			function buildToggler(componentId) {
-				return function() {
-					$mdSidenav(componentId).toggle();
-				};
-			}
-			//sidemenu
-			$scope.sidemenuSections = [
-				{
-					header: 'Purchases',
-					type: 'toogle',
-					icon: 'assets/icons/shopping-cart.svg',
-					pages: [
-						{
-							title: 'Purchases Table',
-							type: 'link',
-							linkTo: '#!/purchases'
-						},
-						{
-							title: 'Status',
-							type: 'link',
-							linkTo: '#!/purchase-status'
-						},
-						{
-							title: 'Type',
-							type: 'link',
-							linkTo: '#!/purchase-types'
-						}
-					]
-				},
-				{
-					header: 'Projects',
-					type: 'toogle',
-					icon: 'assets/icons/folder.svg',
-					pages: [
-						{
-							title: 'Projects Table',
-							type: 'link',
-							linkTo: '#!/projects'
-						},
-						{
-							title: 'Type',
-							type: 'link',
-							linkTo: '#!/project-types'
-						}
-					]
-				},
-				{
-					header: 'Clients',
-					type: 'toogle',
-					icon: 'assets/icons/clipboard-account.svg',
-					pages: [
-						{
-							title: 'Clients Table',
-							type: 'link',
-							linkTo: '#!/clients'
-						},
-						{
-							title: 'Type',
-							type: 'link',
-							linkTo: '#!/client-types'
-						}
-					]
-				},
-				{
-					header: 'Supplier',
-					type: 'link',
-					linkTo: '#!/suppliers',
-					icon: 'assets/icons/box.svg',
-					pages: []
-				},
-				{
-					header: 'Staff',
-					type: 'link',
-					linkTo: '#!/employees',
-					icon: 'assets/icons/account-multiple.svg',
-					pages: []
-				},
-				{
-					header: 'Users',
-					type: 'link',
-					linkTo: '#!/users',
-					icon: 'assets/icons/account-settings.svg',
-					pages: []
-				}
-			];
-		});
+	angular.module('AccountingFEapp', [
+		'ngAnimate',
+		'ngMaterial',
+		'ngRoute',
+		'ngMessages',
+		'angularMoment',
+		'mdColorMenu',
+		'accordion',
+		'validate',
+		'md.data.table',
+		'common',
+		'authForm',
+		'accountInfo',
+		'purchaseTable',
+		'purchaseForm',
+		'purchaseTypeTable',
+		'purchaseStatusTable',
+		'projectTable',
+		'projectTypeTable',
+		'clientTable',
+		'clientTypeTable',
+		'supplierTable',
+		'employeeTable',
+		'userTable'
+	]);
 })();
